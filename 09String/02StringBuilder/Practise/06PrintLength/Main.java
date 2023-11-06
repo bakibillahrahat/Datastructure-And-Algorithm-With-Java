667d56eeb86d272c147765718e40078962040159
bakibillahrahat <bakibillahrahat@protonmail.com>
2023-11-06 23:54:01 +0600

    print length of string

---
+++ 09String/02StringBuilder/Practise/06PrintLength/Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+
+public class Main {
+   public static void main(String args[]) {
+     StringBuilder sb = new StringBuilder("Tony");
+     sb.append(" Stark");
+     System.out.println(sb); 
+
+
+     System.out.println(sb.length());
+   }
+}
