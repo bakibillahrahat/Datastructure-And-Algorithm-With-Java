a47d621f6819497b41d7bb9dc8ac91c7b87f0cbf
MD. Bakibillah Rahat <bakibillahrahat@protonmail.com>
2023-12-12 23:22:33 +0600

    change position

--- 10BitManipulation/01Basic/Main.java
+++ 10BitManipulation/01Basic/Main.java
@@ -3,11 +3,11 @@ import java.util.*;
 public class Bits {
     public static void main(String args[]){
         int n = 5;
-        int post = 2;
+        int post = 3;
         int bitMask = 1<<pos;
 
         if((bitMask & n) == 0) {
-            System.out.println("bit was zero");
+            System.out.println("Bit was zero");
         } else {
             System.out.println("Bit was one");
         }
