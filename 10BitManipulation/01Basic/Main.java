b288ed586a74c54adf1e45587adb85182b091ff8
MD. Bakibillah Rahat <bakibillahrahat@protonmail.com>
2024-04-27 05:53:59 +0600

    practise Bitmanipulation

--- 10BitManipulation/01Basic/Main.java
+++ 10BitManipulation/01Basic/Main.java
@@ -1,15 +1,44 @@
 import java.util.*;
 
 public class Main {
-    public static void main(String args[]){
-        int n = 5;
-        int post = 3;
+    // Create Function to Get Bit of a number at a given position
+    public static void getBit(int n, int pos){
         int bitMask = 1<<pos;
-
-        if((bitMask & n) == 0) {
+        if((bitMask & n) == 0){
             System.out.println("Bit was zero");
-        } else {
+        }else{
             System.out.println("Bit was one");
         }
+    }
+    // Create Function to Set Bit of a number at a given position
+    public static void setBit(int n, int pos){
+        int bitMask = 1<<pos;
+        int newNumber = bitMask | n;
+        System.out.println(newNumber);
+    }
+    // Clear the 3rd bit (position = 2) of a number n.(n = 0101)
+    public static void clearBit(int n, int pos){
+        int bitMask = ~(1<<pos);
+        int newNumber = bitMask & n;
+        System.out.println(newNumber);
+    }
+    // Update the 3rd bit (position = 2) of a number n to 1.(n = 0101)
+    public static void updateBit(int n, int pos, int value){
+        int newNumber;
+        if(value == 0){
+            clearBit(n, pos);
+        }else{
+            setBit(n, pos);
+        }
+    }
+    public static void main(String args[]){
+        // Get Bit of a number at a given position
+        getBit(3, 0);
+        // Set Bit of a number at a given position
+        setBit(5, 1);
+        // Clear the 3rd bit (position = 2) of a number n.(n = 0101)
+        clearBit(5, 2);
+        // Update the 3rd bit (position = 2) of a number n to 1.(n = 0101)
+        updateBit(5, 1, 1);
     }
 }
